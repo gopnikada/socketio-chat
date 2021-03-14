551d7aed1355475bd4665e3f261c2e728d26e25c
gopnikada <klarsky.kirill6@gmail.com>
2021-03-14 13:10:21 +0100

    seems to cors was done

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,12 +2,28 @@ import React, {useState, useEffect} from 'react'
 import queryString from 'querystring'
 import io from 'socket.io-client'
 
+let socket;
+
 const Chat = ({location}) =>{
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = 'localhost:5000'
     useEffect(()=>{
+        let connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity",
+            "timeout" : 10000,
+            "transports" : ["websocket"]
+        };
         location.search = location.search[0] === '?' ? location.search.substr(1,location.search.length) : location.search;
         const {name, room} = queryString.parse(location.search)
-        console.log(name, room)
-    })
+
+        socket = io.connect(ENDPOINT,connectionOptions )
+
+        setName(name)
+        setRoom(room)
+        console.log(socket)
+    }, [ENDPOINT, location.search])
 
     return (
         <h1>Chat</h1>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const cors = require('cors')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, {
+    cors: {
+        origin: "your origin",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
 
 const PORT = process.env.PORT || 5000
 const router = require('./router')
@@ -16,6 +23,8 @@ io.on('connection', socket =>{
     })
 })
 
+app.use(cors());
 app.use(router)
+
 
 server.listen(PORT, ()=>console.log(`server started at ${PORT}`))
